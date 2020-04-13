072d9392c416618eb77a96ea3af32a1e21fa05ae
Daniel Schweiger <daniel.schweiger@coinfinity.co>
2020-04-13 17:53:03 +0200

    Display users

--- lib/widgets/homeWidget.dart
+++ lib/widgets/homeWidget.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_portfolio/widgets/userWidget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,27 +13,14 @@ class HomeWidget extends StatefulWidget {
 
 class _HomeWidgetState extends State<HomeWidget> {
 
-//  for (User user in await UserDatabaseService().getAllUsers()) {
-//  print(user.toString());
-//  }
-//
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome',
-            ),
-          ],
-        ),
-      ) // This trailing comma makes auto-formatting nicer for build methods.
+      body:
+      UserWidget()
     );
   }
 }
