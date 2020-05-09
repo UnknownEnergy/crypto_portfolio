02e33a48fe688095c0392650c234db8e4556d58f
Daniel Schweiger <daniel.schweiger@coinfinity.co>
2020-05-09 23:38:13 +0200

    Create dashboardWidget

--- lib/widgets/dashboardWidget.dart
+++ lib/widgets/dashboardWidget.dart
@@ -1,19 +1,63 @@
+import 'package:crypto_portfolio/models/coin.dart';
+import 'package:crypto_portfolio/models/portfolioCoin.dart';
+import 'package:crypto_portfolio/services/coinDatabase.dart';
+import 'package:crypto_portfolio/services/portfolioCoinDatabase.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 
 class DashboardWidget extends StatelessWidget {
-  Map<String, double> dataMap = new Map();
-
   @override
   Widget build(BuildContext context) {
-    dataMap.putIfAbsent("Flutter", () => 5);
-    dataMap.putIfAbsent("React", () => 3);
-    dataMap.putIfAbsent("Xamarin", () => 2);
-    dataMap.putIfAbsent("Ionic", () => 2);
-
-    return Center(
-      child: PieChart(dataMap: dataMap),
-    );
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          buildDashboardChart(),
+        ]);
   }
+}
+
+FutureBuilder<List<dynamic>> buildDashboardChart() {
+  return FutureBuilder(
+    builder: (context, AsyncSnapshot<List<dynamic>> snap) {
+      if (snap.connectionState == ConnectionState.none ||
+          snap.data == null ||
+          snap.data.length != 2) {
+        return Container();
+      }
+      Map<String, double> dataMap = new Map();
+      List<PortfolioCoin> portfolioCoins = snap.data[0]
+//          .where((portfolioCoin) => portfolioCoin.portfolioId == portfolioId)
+          .toList();
+
+      if (portfolioCoins.length == 0) {
+        dataMap.putIfAbsent("None", () => 100);
+      } else {
+        List<Coin> coins = snap.data[1];
+        double totalPercent = portfolioCoins
+            .map((portfolioCoin) => portfolioCoin.percent)
+            .reduce((sum, element) => sum + element);
+        coins.forEach((coin) {
+          double coinPercent = 0;
+
+          List<double> percentages = portfolioCoins
+              .where((portfolioCoin) => portfolioCoin.coinId == coin.id)
+              .map((portfolioCoin) => portfolioCoin.percent).toList();
+
+          if(percentages.length > 0) {
+            coinPercent = percentages.reduce((sum, element) => sum + element);
+          }
+
+          dataMap.putIfAbsent(
+              coin.name, () => coinPercent / totalPercent * 100);
+        });
+      }
+      return PieChart(dataMap: dataMap);
+    },
+    future: Future.wait([
+      new PortfolioCoinDatabaseService().getAllPortfolioCoins(),
+      new CoinDatabaseService().getAllCoins()
+    ]),
+  );
 }
